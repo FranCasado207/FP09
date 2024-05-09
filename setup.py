12ddac879a264a2b4d393c4db5b5ab7e11d2ef18
FranCasado207 <fran.casado207@gmail.com>
2024-05-09 20:50:07 +0100

    a

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='mod_FranciscoCasado',
+    name='FP09FranciscoCasado',
     version='0.2.0',
     packages=find_packages(['mod_FranciscoCasado']),
 )
